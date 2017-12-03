b3684134919a8e513605ba5d3f461305dff670f5
Frederic Lepied <flepied@gmail.com>
2017-12-03 11:32:32 +0100

    monitor_trade.py: refactored duplicated code to send orders

--- monitor_trade.py
+++ monitor_trade.py
@@ -9,6 +9,16 @@ def get_orders(conn, market):
     return conn.get_open_orders(market)
 
 
+def send_order(order, exch, func, *args, **kwargs):
+    if order and exch.cancel_order(order):
+        order = None
+    new_order = func(*args, **kwargs)
+    if new_order:
+        print(new_order.data)
+        order = new_order
+    return order
+
+
 if len(sys.argv) != 6:
     print('Usage: %s <market> <quantity> <stop> <entry> <exit>' % sys.argv[0])
     sys.exit(1)
@@ -40,26 +50,19 @@ print(trend)
 while True:
     tick = exch.get_tick(market)
     if tick:
-        print(tick)
+        print(market, tick)
         # TODO(fl): need to abstract tick
-        if tick['C'] < entry:
+        last = tick['C']
+        if last < entry:
             if trend != 'down':
                 print('down')
-                if order and exch.cancel_order(order):
-                    order = None
-                new_order = exch.sell_stop(market, quantity, stop)
-                if new_order:
-                    print(new_order)
-                    order = new_order
+                order = send_order(order, exch, exch.sell_stop,
+                                   market, quantity, stop)
                 trend = 'down'
         else:
             if trend != 'up':
                 print('up')
-                if order and exch.cancel_order(order):
-                    order = None
-                new_order = exch.sell_limit(market, quantity, exit)
-                if new_order:
-                    print(new_order)
-                    order = new_order
+                order = send_order(order, exch, exch.sell_limit,
+                                   market, quantity, exit)
                 trend = 'up'
     time.sleep(60)
