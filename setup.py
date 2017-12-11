495aad949fcf5155d595d77a8ad5b33d0f28ca0a
Frederic Lepied <flepied@gmail.com>
2017-12-11 18:37:54 +0100

    provide a simple tox.ini to run unit tests and flake8

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tbot',
+      version='0.1',
+      description='tbot',
+      author='Frederic Lepied',
+      author_email='flepied@gmail.com',
+      )
